6dba91a456520c70f18a0e760b0194c3f4b8e1a3
amol.katte@wellnessforever.in <amol.katte@wellnessforever.in>
2025-11-25 13:13:01 +0530

    test

--- pathlabmaster/src/main/java/pathlabmaster/rest/UserRestController.java
+++ pathlabmaster/src/main/java/pathlabmaster/rest/UserRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
 	@GetMapping("/add")
 	public String sayHello() {
-		return "Hello, World! Amol";
+		return "Hello, World! Amol!!!!!!!!!";
 	}
 
 

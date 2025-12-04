7317a569cc32016a78e4cdb6a355ed8ae54c588d
amol.katte@wellnessforever.in <amol.katte@wellnessforever.in>
2025-12-04 12:25:55 +0530

    frontend url config for cross platform issue

--- pathlabmaster/src/main/java/pathlabmaster/utility/CorsConfig.java
+++ pathlabmaster/src/main/java/pathlabmaster/utility/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174","https://path-lab-master.onrender.com")
+                        .allowedOrigins("http://localhost:5174","https://path-lab-master-frontend.vercel.app")
                         .allowedMethods("*")
                         .allowCredentials(true);
             }
